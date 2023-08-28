0ec8c11953addf124568f68be224ce06eae19e0a
Mikayel <mikayel1999@mail.com>
2023-08-28 17:15:38 +0000

    Update App.js, Refs to Select DOM Elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -162,6 +162,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function(el) {
+
+    function callback(e) {
+      if(e.code === 'Enter') {
+        if(document.activeElement === inputEl.current) {
+          return
+        } 
+        inputEl.current.focus()
+        setQuery('');
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => {
+      document.removeEventListener("keydown", callback);
+    }
+  }, [setQuery])
+
   return (
     <input
       className="search"
@@ -169,6 +189,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
